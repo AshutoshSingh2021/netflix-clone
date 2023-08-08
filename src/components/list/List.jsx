e56421c9b695ca789bba023dfe72504f3535e906
AshutoshSingh2021 <ashutoshsingh15102@gmail.com>
2023-08-08 10:19:21 +0530

    Inplemented slider in the list items

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./list.scss";
 import {
   ArrowBackIosNewRounded,
@@ -7,25 +7,50 @@ import {
 import ListItem from "../listitem/ListItem";
 
 export default function List() {
+  const [isMoved, setIsMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+
+  const listRef = useRef();
+
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    if (direction === "right" && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+  };
+
   return (
     <div className="list">
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <ArrowBackIosNewRounded className="sliderArrow left" />
-        <div className="container">
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
+        <ArrowBackIosNewRounded
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved && "none" }}
+        />
+        <div className="container" ref={listRef}>
+          <ListItem index={0} />
+          <ListItem index={1} />
+          <ListItem index={2} />
+          <ListItem index={3} />
+          <ListItem index={4} />
+          <ListItem index={5} />
+          <ListItem index={6} />
+          <ListItem index={7} />
+          <ListItem index={8} />
+          <ListItem index={9} />
+          <ListItem index={10} />
         </div>
-        <ArrowForwardIosRounded className="sliderArrow right" />
+        <ArrowForwardIosRounded
+          className="sliderArrow right"
+          onClick={() => handleClick("right")}
+        />
       </div>
     </div>
   );
--- src/components/listitem/ListItem.jsx
+++ src/components/listitem/ListItem.jsx
@@ -1,6 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./listItem.scss";
+import ListItemImg from "../../asset/item.jpg";
+import {
+  Add,
+  PlayArrow,
+  ThumbDownOutlined,
+  ThumbUpAltOutlined,
+} from "@mui/icons-material";
 
-export default function ListItem() {
-  return <div className="listItem">item</div>;
+export default function ListItem(props) {
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <div
+      className="listItem"
+      style={{ left: isHovered && props.index * 225 - 50 + props.index * 2.5 }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img src={ListItemImg} alt="" />
+      <div className="itemInfo">
+        <div className="icons">
+          <PlayArrow />
+          <Add />
+          <ThumbUpAltOutlined />
+          <ThumbDownOutlined />
+        </div>
+        <div className="itemInfoTop">
+          <span>1 hour 14 mins</span>
+          <span className="limit">+16</span>
+          <span>2020</span>
+        </div>
+        <div className="desc">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis
+          maxime possimus sed voluptas corporis fugiat corrupti facere ullam?
+          Laborum, labore.
+        </div>
+        <div className="genre">Action</div>
+      </div>
+    </div>
+  );
 }
