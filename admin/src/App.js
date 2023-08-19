9a15e291c5b05866df074437f0f43bb8221e6ccc
AshutoshSingh2021 <ashutoshsingh15102@gmail.com>
2023-08-19 09:58:36 +0530

    configuring admin pannel

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,6 +1,6 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
-import "./App.css";
+import "./app.css";
 import Home from "./pages/home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
@@ -9,8 +9,45 @@ import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const MONTHS = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const [userStats, setUserStats] = useState([]);
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await axios.get("/users/stats", {
+          headers: {
+            token:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZDc1ZjRiM2M0YjA3NDIyYzIwZjZhMCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY5MTg5OTIyNSwiZXhwIjoxNjkyMzMxMjI1fQ.VrlzU6U7jHG9SmTqVAL2abubpIHWwMNf7HIxH_lm3qU",
+          },
+        });
+        setUserStats(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getStats();
+  }, []);
+
+  console.log(userStats);
   return (
     <Router>
       <Topbar />
